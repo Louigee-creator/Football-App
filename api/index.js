3591957e8a06116d2518277da5eb6f5322eadbeb
Louis Acheampong <louisacheampong666@gmail.com>
2022-09-28 12:42:19 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ export const getFixturesData = async () => {
 const news = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'c132740728mshb083468ea77438ep1510bfjsn99bc274f1e08',
+    'X-RapidAPI-Key': 'xxxxxxxxxxxxxxxxxxx', // your key here
     'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
   }
 };
@@ -45,24 +45,6 @@ export const getNewsData = async () => {
 }
 
 
-// const detailedNews = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': 'c132740728mshb083468ea77438ep1510bfjsn99bc274f1e08',
-//     'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
-//   }
-// };
-
-// export const getDetailedNewsData = async (newsId) => {
-//   try {
-//       const {data : { topStories }} = await axios.get(DetailedNewsURL, detailedNews);
-
-//       return topStories;
-//     } catch (error) {
-//       console.log(error)
-//     }
-//     console.log(topStories)
-// }
 
 
 
@@ -71,7 +53,7 @@ const results = {
   method: 'GET',
   params: {Category: 'soccer'},
   headers: {
-    'X-RapidAPI-Key': 'c132740728mshb083468ea77438ep1510bfjsn99bc274f1e08',
+    'X-RapidAPI-Key': 'xxxxxxxxxxxxxxxxx', //your key here
     'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
   }
 };
